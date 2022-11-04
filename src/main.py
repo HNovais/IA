836c8e440617d398396333c6711e079ea99d5a03
Hugo Novais <73346868+HNovais@users.noreply.github.com>
2022-11-03 20:30:54 -0400

    Update na função de criar ligações

--- src/main.py
+++ src/main.py
@@ -4,25 +4,25 @@ def main():
     # Criar instância de grafo
     g = Grafo()
 
-    file = open("maze.txt", "r")
+    # Abrir ficheiro .txt com a pista
+    file = open(r"d:\Universidade\IA\Trabalho\IA-main\src\maze.txt","r")
     maze = file.readlines()
-    print(maze)
-    file.close
 
-    j=0
-
-    while j<7:
-        i=0
-        while i<10:
-            if i<9:
+    # Criar ligações entre nodos
+    j = 0
+    for line in maze:
+        i = 0
+        for char in line[:-1]:
+            if i < len(line) - 2:
                 g.add_edge(maze[j][i], i, j, maze[j][i+1], i+1, j)
-            if j<6:
+            if j < len(maze) - 1:
                 g.add_edge(maze[j][i], i, j, maze[j+1][i], i, j+1)
-            i=i+1
-        j=j+1
+            i = i + 1
+        j = j + 1
 
-    print(len(g.grafo.keys()))
     print(g)
+
+    file.close()
 
 if __name__ == "__main__":
     main()
